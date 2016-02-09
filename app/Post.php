1a5f4489bd8c550bf0e58d6d9faf476ce01acc80
MrPaulPhan <paul@mrpaulphan.com>
2016-02-08 22:06:35 -0500

    Relating users and posts

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-  protected $fillable = ['body'];
+    protected $fillable = ['body'];
+
+    protected $appends = ['humanCreatedAt'];
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+
+    public function getHumanCreatedAtAttribute()
+    {
+        return $this->created_at->diffForHumans();
+    }
 }
--- app/User.php
+++ app/User.php
@@ -6,21 +6,75 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
     protected $fillable = [
         'username', 'email', 'password',
     ];
 
-    /**
-     * The attributes excluded from the model's JSON form.
-     *
-     * @var array
-     */
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    protected $appends = [
+        'avatar',
+        'profileUrl',
+    ];
+
+    public function posts()
+    {
+        return $this->hasMany('App\Post');
+    }
+
+    public function getAvatar()
+    {
+        return 'https://www.gravatar.com/avatar/' . md5($this->email) . '?s=45&d=mm';
+    }
+
+    public function getAvatarAttribute()
+    {
+        return $this->getAvatar();
+    }
+
+    public function getProfileUrlAttribute()
+    {
+        return route('user.index', $this);
+    }
+
+    public function getRouteKeyName()
+    {
+        return 'username';
+    }
+
+    public function isNot(User $user)
+    {
+        return $this->id !== $user->id;
+    }
+
+    public function isFollowing(User $user)
+    {
+        return (bool) $this->following->where('id', $user->id)->count();
+    }
+
+    public function canFollow(User $user)
+    {
+        if (!$this->isNot($user)) {
+            return false;
+        }
+
+        return !$this->isFollowing($user);
+    }
+
+    public function canUnfollow(User $user)
+    {
+        return $this->isFollowing($user);
+    }
+
+    public function following()
+    {
+        return $this->belongsToMany('App\User', 'follows', 'user_id', 'follower_id');
+    }
+
+    public function followers()
+    {
+        return $this->belongsToMany('App\User', 'follows', 'follower_id', 'user_id');
+    }
 }
